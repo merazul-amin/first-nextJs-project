25b01343b1a0f813b119a6312b6c3ea5b760d281
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-03-31 19:43:49 +0600

    error page addedg

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>this is Build in error page</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
